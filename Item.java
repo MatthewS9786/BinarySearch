01a60ccdb48dff7f34437576d9f1d2aa497820b0
MatthewS9786 <70603206+MatthewS9786@users.noreply.github.com>
2021-03-18 16:00:05 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,11 +1,1 @@
-public class Item
-{
-    private int myCatNum, myInventory;
-    public Item(int nNum, int nInv)
-    {
-        myCatNum = nNum;
-        myInventory = nInv;
-    }
-    public int getCatNum(){return myCatNum;}
-    public int getInventory(){return myInventory;}
-}
+
